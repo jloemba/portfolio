d46090090b331c47eca717cda7942df36de3206a
Joris Loemba <jorisloemba@gmail.com>
2026-02-18 15:23:40 +0100

    fix : change base name to load css

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jloemba.github.io',
-  base: '/',
+  base: '/portfolio/',
   //integrations: [github()],
   output: 'static',
   vite: {
