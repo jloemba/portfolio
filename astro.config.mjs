6d25e571b95a10c058ff04d450f879309da3d7f0
Joris Loemba <jorisloemba@gmail.com>
2026-02-18 15:17:22 +0100

    update astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+//import github from 'astrojs/github-pages';
+
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jloemba.github.io',
+  base: '/',
+  //integrations: [github()],
+  output: 'static',
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
